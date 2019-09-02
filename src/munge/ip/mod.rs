4bee560ac997f3129f0c2ca715143086a3d63625
Christopher Bunn <b11.chris@gmail.com>
2019-09-02 13:47:43 -0700

    [ip] IPMunger unit tests

--- src/munge/ip/mod.rs
+++ src/munge/ip/mod.rs
@@ -3,44 +3,6 @@ use std::io::Write;
 use std::net::Ipv6Addr;
 use std::str::FromStr;
 
-#[cfg(test)]
-mod tests {
-    //TODO: figure out the typical rust naming and use conventions
-    use super::{compact, explode, maybe_ip};
-    use std::net::Ipv6Addr;
-
-    #[test]
-    fn maybe_ip_positive() {
-        assert!(maybe_ip("1::1").is_some());
-        assert!(maybe_ip("::1").is_some());
-        assert!(maybe_ip("a:a:a:a:a:a:a:a").is_some());
-        assert!(maybe_ip("a:a::a").is_some());
-        assert!(maybe_ip("A::1").is_some());
-    }
-    #[test]
-    fn maybe_ip_negative() {
-        assert!(maybe_ip("a:a:a:a:a:a:a:a:a").is_none());
-        assert!(maybe_ip("a:a:a:a:a:a:a").is_none());
-        assert!(maybe_ip("::g").is_none());
-        assert!(maybe_ip("a:z::a").is_none());
-    }
-    #[test]
-    fn explode_ip() {
-        let mut buf = Vec::new();
-        explode(&Ipv6Addr::new(0, 0, 0xaf77, 0, 0, 0, 0, 1), &mut buf);
-        assert_eq!(
-            "0000:0000:af77:0000:0000:0000:0000:0001",
-            String::from_utf8(buf).unwrap()
-        )
-    }
-    #[test]
-    fn compact_ip() {
-        let mut buf = Vec::new();
-        compact(&Ipv6Addr::new(0, 0, 0xaf77, 0, 0, 0, 0, 1), &mut buf);
-        assert_eq!("0:0:af77::1", String::from_utf8(buf).unwrap())
-    }
-}
-
 pub enum IPFormat {
     Compact,
     Exploded,
@@ -61,6 +23,7 @@ fn maybe_ip(line: &str) -> Option<Ipv6Addr> {
         Err(_) => None,
     }
 }
+
 fn explode<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
     match ip.segments() {
         [a, b, c, d, e, f, g, h] => write!(
@@ -71,6 +34,7 @@ fn explode<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
         .unwrap(),
     };
 }
+
 fn compact<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
     write!(loc, "{}", ip).unwrap();
 }
@@ -78,7 +42,7 @@ fn compact<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
 impl Munger for IPMunger<'_> {
     fn possible_match(&self, c: char) -> bool {
         match c {
-            '0'..='9' | 'a'..='f' | ':' => true,
+            '0'..='9' | 'a'..='f' | 'A'..='F' | ':' => true,
             _ => false,
         }
     }
@@ -91,8 +55,105 @@ impl Munger for IPMunger<'_> {
             None => self.writethru(s),
         };
     }
-
     fn writethru(&mut self, s: &str) {
         write!(&mut self.out, "{}", s).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    //TODO: figure out the typical rust naming and use conventions
+    use super::Munger;
+    use super::{compact, explode, maybe_ip, IPFormat, IPMunger, IPMungerConfig};
+    use std::net::Ipv6Addr;
+
+    #[test]
+    fn test_rewriter() {
+        let mut buf = Vec::new();
+        let mut m = IPMunger {
+            out: &mut buf,
+            config: IPMungerConfig {
+                format: IPFormat::Compact,
+            },
+        };
+
+        m.rewriter("foo");
+        m.rewriter(" ");
+        m.rewriter("0000:0000:af77:0000:0000:0000:0000:0001");
+        m.rewriter(" ");
+        m.rewriter("0:0::0:1");
+        assert_eq!("foo 0:0:af77::1 ::1", String::from_utf8(buf).unwrap());
+    }
+
+    #[test]
+    fn test_writethru() {
+        let mut buf = Vec::new();
+        let mut m = IPMunger {
+            out: &mut buf,
+            config: IPMungerConfig {
+                format: IPFormat::Compact,
+            },
+        };
+
+        m.writethru("foo");
+        assert_eq!("foo", String::from_utf8(buf).unwrap());
+    }
+
+    #[test]
+    fn test_possible_match() {
+        let m = IPMunger {
+            out: &mut Vec::new(),
+            config: IPMungerConfig {
+                format: IPFormat::Compact,
+            },
+        };
+
+        assert!(m.possible_match('a'));
+        assert!(m.possible_match('A'));
+        assert!(m.possible_match('f'));
+        assert!(m.possible_match('F'));
+        assert!(m.possible_match('0'));
+        assert!(m.possible_match('9'));
+        assert!(m.possible_match(':'));
+
+        assert!(!m.possible_match('g'));
+        assert!(!m.possible_match('G'));
+        assert!(!m.possible_match('_'));
+        assert!(!m.possible_match('!'));
+    }
+
+    #[test]
+    fn test_maybe_ip() {
+        // positive cases
+        assert!(maybe_ip("1::1").is_some());
+        assert!(maybe_ip("::1").is_some());
+        assert!(maybe_ip("a:a:a:a:a:a:a:a").is_some());
+        assert!(maybe_ip("a:a::a").is_some());
+        assert!(maybe_ip("A::1000").is_some());
+        assert!(maybe_ip("A:f::1").is_some());
+
+        // negative cases
+        assert!(maybe_ip("a:a:a:a:a:a:a:a:a").is_none());
+        assert!(maybe_ip("a:a:a:a:a:a:a").is_none());
+        assert!(maybe_ip("::g").is_none());
+        assert!(maybe_ip("::10001").is_none());
+        assert!(maybe_ip("a:z::a").is_none());
+    }
+
+    #[test]
+    fn test_explode() {
+        let mut buf = Vec::new();
+        explode(&Ipv6Addr::new(0, 0, 0xaf77, 0, 0, 0, 0, 1), &mut buf);
+        assert_eq!(
+            "0000:0000:af77:0000:0000:0000:0000:0001",
+            String::from_utf8(buf).unwrap()
+        )
+    }
+
+    #[test]
+    fn test_compact() {
+        let mut buf = Vec::new();
+        compact(&Ipv6Addr::new(0, 0, 0xaf77, 0, 0, 0, 0, 1), &mut buf);
+        assert_eq!("0:0:af77::1", String::from_utf8(buf).unwrap())
     }
 }
