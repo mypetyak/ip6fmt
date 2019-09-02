85725c9edfab272c971745f3ee2087c6c6fea149
Christopher Bunn <b11.chris@gmail.com>
2019-09-02 13:10:08 -0700

    [ip] format and matching unit tests

--- src/munge/ip/mod.rs
+++ src/munge/ip/mod.rs
@@ -3,6 +3,44 @@ use std::io::Write;
 use std::net::Ipv6Addr;
 use std::str::FromStr;
 
+#[cfg(test)]
+mod tests {
+    //TODO: figure out the typical rust naming and use conventions
+    use super::{compact, explode, maybe_ip};
+    use std::net::Ipv6Addr;
+
+    #[test]
+    fn maybe_ip_positive() {
+        assert!(maybe_ip("1::1").is_some());
+        assert!(maybe_ip("::1").is_some());
+        assert!(maybe_ip("a:a:a:a:a:a:a:a").is_some());
+        assert!(maybe_ip("a:a::a").is_some());
+        assert!(maybe_ip("A::1").is_some());
+    }
+    #[test]
+    fn maybe_ip_negative() {
+        assert!(maybe_ip("a:a:a:a:a:a:a:a:a").is_none());
+        assert!(maybe_ip("a:a:a:a:a:a:a").is_none());
+        assert!(maybe_ip("::g").is_none());
+        assert!(maybe_ip("a:z::a").is_none());
+    }
+    #[test]
+    fn explode_ip() {
+        let mut buf = Vec::new();
+        explode(&Ipv6Addr::new(0, 0, 0xaf77, 0, 0, 0, 0, 1), &mut buf);
+        assert_eq!(
+            "0000:0000:af77:0000:0000:0000:0000:0001",
+            String::from_utf8(buf).unwrap()
+        )
+    }
+    #[test]
+    fn compact_ip() {
+        let mut buf = Vec::new();
+        compact(&Ipv6Addr::new(0, 0, 0xaf77, 0, 0, 0, 0, 1), &mut buf);
+        assert_eq!("0:0:af77::1", String::from_utf8(buf).unwrap())
+    }
+}
+
 pub enum IPFormat {
     Compact,
     Exploded,
@@ -17,26 +55,24 @@ pub struct IPMunger<'a> {
     pub config: IPMungerConfig,
 }
 
-impl IPMunger<'_> {
-    fn maybe_ip(line: &str) -> Option<Ipv6Addr> {
-        match Ipv6Addr::from_str(&line) {
-            Ok(v) => Some(v),
-            Err(_) => None,
-        }
+fn maybe_ip(line: &str) -> Option<Ipv6Addr> {
+    match Ipv6Addr::from_str(&line) {
+        Ok(v) => Some(v),
+        Err(_) => None,
     }
-    fn explode<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
-        match ip.segments() {
-            [a, b, c, d, e, f, g, h] => write!(
-                loc,
-                "{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}",
-                a, b, c, d, e, f, g, h
-            )
-            .unwrap(),
-        };
-    }
-    fn compact<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
-        write!(loc, "{}", ip).unwrap();
-    }
+}
+fn explode<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
+    match ip.segments() {
+        [a, b, c, d, e, f, g, h] => write!(
+            loc,
+            "{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}:{:0>4x}",
+            a, b, c, d, e, f, g, h
+        )
+        .unwrap(),
+    };
+}
+fn compact<T: Write>(ip: &Ipv6Addr, loc: &mut T) {
+    write!(loc, "{}", ip).unwrap();
 }
 
 impl Munger for IPMunger<'_> {
@@ -47,10 +83,10 @@ impl Munger for IPMunger<'_> {
         }
     }
     fn rewriter(&mut self, s: &str) {
-        match IPMunger::maybe_ip(&s) {
+        match maybe_ip(&s) {
             Some(ip) => match self.config.format {
-                IPFormat::Exploded => IPMunger::explode(&ip, &mut self.out),
-                IPFormat::Compact => IPMunger::compact(&ip, &mut self.out),
+                IPFormat::Exploded => explode(&ip, &mut self.out),
+                IPFormat::Compact => compact(&ip, &mut self.out),
             },
             None => self.writethru(s),
         };
